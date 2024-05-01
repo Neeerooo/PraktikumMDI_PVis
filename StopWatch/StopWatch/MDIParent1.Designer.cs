d0e7f801a142187f4028c53bf0bbf62ac6428407
Nero24 <153308025+Neeerooo@users.noreply.github.com>
2024-05-01 21:27:48 +0700

    Add files via upload

---
+++ StopWatch/StopWatch/MDIParent1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace StopWatch
+{
+    partial class MDIParent1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pilihMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopWatchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.digitalClockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pilihMenuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(843, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pilihMenuToolStripMenuItem
+            // 
+            this.pilihMenuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopWatchToolStripMenuItem,
+            this.digitalClockToolStripMenuItem});
+            this.pilihMenuToolStripMenuItem.Name = "pilihMenuToolStripMenuItem";
+            this.pilihMenuToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.pilihMenuToolStripMenuItem.Text = "Pilih menu";
+            // 
+            // stopWatchToolStripMenuItem
+            // 
+            this.stopWatchToolStripMenuItem.Name = "stopWatchToolStripMenuItem";
+            this.stopWatchToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.stopWatchToolStripMenuItem.Text = "Stop watch";
+            this.stopWatchToolStripMenuItem.Click += new System.EventHandler(this.stopWatchToolStripMenuItem_Click);
+            // 
+            // digitalClockToolStripMenuItem
+            // 
+            this.digitalClockToolStripMenuItem.Name = "digitalClockToolStripMenuItem";
+            this.digitalClockToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.digitalClockToolStripMenuItem.Text = "Digital clock";
+            this.digitalClockToolStripMenuItem.Click += new System.EventHandler(this.digitalClockToolStripMenuItem_Click);
+            // 
+            // MDIParent1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 558);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MDIParent1";
+            this.Text = "MDIParent1";
+            this.Load += new System.EventHandler(this.MDIParent1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pilihMenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopWatchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem digitalClockToolStripMenuItem;
+    }
+}
+
+
+
